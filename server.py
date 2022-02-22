2cd35eedaa626785db17389b6080b42022e1539b
Arjun Syam <45396214+Hu4k3n@users.noreply.github.com>
2022-02-23 02:47:17 +0530

    Completed Aether Address Server

---
+++ server.py
@@ -0,0 +1,32 @@
+# Dictionary containing sample names and their corresponding keys
+import sample_names_dictionary as sn
+import sys
+from flask import Flask, jsonify, request, redirect, url_for
+app = Flask(__name__)
+
+# To search whether the key is already assigned to a name 
+def isKeyPresant(key):
+    length = len(sn.name_Dict)
+    for usr, curr_key in sn.name_Dict.items():
+        if key == curr_key:
+            return usr
+    return None
+
+@app.route("/", methods=["GET", "POST"])
+# To find the name of the user based on the key
+def findUsername() :
+    request_data = request.get_json()
+    new_public_key = request_data['publickey']
+    username = isKeyPresant(str(new_public_key))
+    # return 'hi'
+    if username != None :
+        return jsonify({"username": username})
+    else : 
+        for usr, key in sn.name_Dict.items() :
+            if key == None :
+                sn.name_Dict[usr] = str(new_public_key)
+                return jsonify({"username": usr})
+            
+    
+if __name__ == "__main__":
+    app.run()
